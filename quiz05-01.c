d306346fa62ab1c84c1a133bd669b9222f7b3388
David <petzoldavid02@gmail.com>
2022-09-20 13:58:06 -0600

    Problem 1 done

--- quiz05-01.c
+++ quiz05-01.c
@@ -1,25 +1,33 @@
 /** quiz05-01.c
 * ===========================================================
-*    Name: 
-* Section:
+*    Name: David Petzold
+* Section: T 6/7
 * Purpose: Quiz 5
 * ===========================================================
 */
 
- #include <stdio.h>
- 
- int main(void){
-    
+#include <stdio.h>
+
+int main(void){
+
     int firstNum = 0;
     int lastNum = 0;
-    
+
     printf("enter the first number: ");
     scanf("%i", &firstNum);
     printf("enter the last number: ");
     scanf("%i", &lastNum);
 
     // YOUR CODE GOES HERE
+    if (firstNum > lastNum){
+        printf("%d ", firstNum);
+    }
 
+    while (firstNum <= lastNum)
+    {
+        printf("%i ", firstNum);
+        firstNum += 3;
+    }
 
     printf("done\n\n"); // leave this here
     return 0;
