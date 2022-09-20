555719c94958e72b5d6ca8002e279393f9ced359
David <petzoldavid02@gmail.com>
2022-09-20 14:06:57 -0600

    Quiz complete

--- quiz05-03.c
+++ quiz05-03.c
@@ -1,21 +1,26 @@
 /** quiz05-03.c
 * ===========================================================
-*    Name: 
-* Section:
+*    Name: David Petzold
+* Section: T 6/7
 * Purpose: Quiz 5
 * ===========================================================
 */
- 
- #include <stdio.h>
- 
- int main(void){
-            
+
+#include <stdio.h>
+
+int main(void){
+
     int sideLength = 0;
     printf("enter the size: ");
     scanf("%i", &sideLength);
 
     // YOUR CODE GOES HERE
-
+    for (int i = 0; i < sideLength; i++){
+        for (int j = 0; j < sideLength * 3; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
 
     printf("done\n\n"); // leave this here
     return 0;
